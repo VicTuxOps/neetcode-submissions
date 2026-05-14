5fd69985b1e30d3de4b9bb767c5876b61d1eb000
Victor Rod <48866166+VicTuxOps@users.noreply.github.com>
2026-05-14 22:34:58 +0200

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,11 @@
+integer_type=7
+float_type=7.3
+bool_type=True
+string_type="Test"
+list_type=[1,2,3]
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
