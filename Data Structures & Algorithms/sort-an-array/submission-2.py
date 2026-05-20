c9061b4ae22bb742261f565a6b0f4a19c6fa144c
Victor Rod <48866166+VicTuxOps@users.noreply.github.com>
2026-05-20 23:09:15 +0200

    Add: sort-an-array - submission-2

---
+++ Data Structures & Algorithms/sort-an-array/submission-2.py
@@ -0,0 +1,41 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        self.heap = [None] + nums
+        res = []
+        cur = (len(self.heap)-1)//2
+        while cur > 0:
+            i = cur
+            while 2*i < len(self.heap):
+                if (2*i+1 < len(self.heap) and (self.heap[2*i+1] < self.heap[2*i]) and (self.heap[i] > self.heap[2*i+1])):
+                    self.heap[i], self.heap[2*i+1] = self.heap[2*i+1], self.heap[i]
+                    i = 2*i+1
+                elif self.heap[2*i] < self.heap[i]:
+                    self.heap[i], self.heap[2*i] = self.heap[2*i], self.heap[i]
+                    i = 2*i
+                else:
+                    break
+            cur -=1
+        
+        while len(self.heap) > 1:
+            res.append(self.pop())
+        return res
+    
+    def pop(self):
+        if len(self.heap) == 1:
+            return None
+        if len(self.heap) == 2:
+            return self.heap.pop()
+        
+        res = self.heap[1]
+        self.heap[1] = self.heap.pop()
+        i = 1
+        while 2*i < len(self.heap):
+            if (2*i+1 < len(self.heap) and (self.heap[2*i+1] < self.heap[2*i]) and (self.heap[i] > self.heap[2*i+1])):
+                self.heap[i], self.heap[2*i+1] = self.heap[2*i+1], self.heap[i]
+                i = 2*i+1
+            elif self.heap[2*i] < self.heap[i]:
+                self.heap[i], self.heap[2*i] = self.heap[2*i], self.heap[i]
+                i = 2*i
+            else:
+                break
+        return res
