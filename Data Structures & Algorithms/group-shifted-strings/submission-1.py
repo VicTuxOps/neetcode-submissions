fe36d8b8a17bf4c0ef20f211b1322353cd9b1e2f
Victor Rod <48866166+VicTuxOps@users.noreply.github.com>
2026-05-25 21:03:05 +0200

    Add: group-shifted-strings - submission-1

---
+++ Data Structures & Algorithms/group-shifted-strings/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def groupStrings(self, strings: List[str]) -> List[List[str]]:
+        sizeStr = {}
+        for word in strings:
+            idx = self.hashFunction(word)
+            sizeStr.setdefault(idx, []).append(word)
+        
+        return list(sizeStr.values())
+
+    def hashFunction(self, word):
+        res = ()
+        if len(word) >= 1:
+            for i in range(1,len(word)):
+                temp = (ord(word[i]) - ord(word[i-1]))%26
+                res = res + (temp,)
+        return res
