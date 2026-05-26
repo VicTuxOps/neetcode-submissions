b61030aadaea7a3210a3311e7b5a6ef66780a64d
Victor Rod <48866166+VicTuxOps@users.noreply.github.com>
2026-05-26 23:49:48 +0200

    Add: search-in-rotated-sorted-array-ii - submission-6

---
+++ Data Structures & Algorithms/search-in-rotated-sorted-array-ii/submission-6.py
@@ -0,0 +1,23 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        left = 0
+        right = len(nums)-1
+        while left <= right:
+            mid = (right + left)//2
+            if target == nums[mid]:
+                return True
+            if nums[left] == nums[mid]:
+                left+=1
+            elif nums[mid] >= nums[left]:
+                if target < nums[mid] and target >= nums[left]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            else:
+                if target > nums[mid] and target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        
+        return False
+            
