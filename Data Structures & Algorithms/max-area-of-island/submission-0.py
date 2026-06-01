069997906dbe626e9c3be1a91d3a9403f017b692
Victor Rod <48866166+VicTuxOps@users.noreply.github.com>
2026-06-02 00:04:50 +0200

    Add: max-area-of-island - submission-0

---
+++ Data Structures & Algorithms/max-area-of-island/submission-0.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        visit = set()
+        count = 0
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 1 and (r,c) not in visit:
+                    tmp, visit = self.dfs(grid, visit, r, c, ROWS, COLS)
+                    count = max(count,tmp)
+        return count
+    
+    def dfs(self, grid, visit, r, c, ROWS, COLS):
+        if (min(r,c) < 0 or r >= ROWS or c >= COLS or (r,c) in visit or grid[r][c] == 0):
+            return 0, visit
+        if grid[r][c] == 1:
+            visit.add((r,c))
+            count = 1
+            count += self.dfs(grid,visit,r+1,c, ROWS, COLS)[0]
+            count += self.dfs(grid,visit,r-1,c, ROWS, COLS)[0]
+            count += self.dfs(grid,visit,r,c+1, ROWS, COLS)[0]
+            count += self.dfs(grid,visit,r,c-1, ROWS, COLS)[0]
+        #visit.remove((r,c))
+        return count,visit
